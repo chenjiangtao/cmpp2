cb1e55341656350c900eee40f36732700b3d1649
jiangtao <evane1890@gmail.com>
2017-12-18 22:05:54 +0800

    init

--- readme.md
+++ readme.md
@@ -12,3 +12,6 @@ sendMsg(String msg,String cusMsisdn)//发送SMS
 - 4.“定时器.txt”记录的是长链接链路检查的基于spring的配置，如果使用java原生定时器可自行配置。
 
 - 5.依赖包包括log4j.jar、quartz-1.5.2.jar，quartz-1.5.2.jar为定时器使用，除此之外无特殊依赖包
+
+#run 
+java -cp 'lib/*:out/production/cmpp2/:out/test/cmpp2/' Test
--- test/Test.java
+++ test/Test.java
@@ -7,7 +7,9 @@ public class Test {
      * @param args
      */
     public static void main(String[] args) {
-        MsgContainer.sendMsg("中文测试【前潮】", "18621569931");
+//        MsgContainer.sendMsg("中文测试 a【前潮】", "15327921665");
+        MsgContainer.activityTestISMG();
+//        MsgContainer.getSocketInstance();
     }
 
 }
